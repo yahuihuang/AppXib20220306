e882d249f0b0346667f093b2a3614b34a234efa8
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-06 09:55:13 +0800

    建立 UIView - MyView

---
+++ AppXib20220306/MyView.swift
@@ -0,0 +1,20 @@
+//
+//  MyView.swift
+//  AppXib20220306
+//
+//  Created by grace on 2022/3/6.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
